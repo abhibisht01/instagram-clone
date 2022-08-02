d5fdec7f6aaa99c3bb9d84567a347eca255bf522
Abhishek Bisht <99400134+abhibisht01@users.noreply.github.com>
2022-08-02 16:45:26 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 import 'firebase/compat/storage';
 const firebaseConfig = {
-    apiKey: "AIzaSyD9Fs2x-d-D5ctH2ljtovbb1vbXk_cegpg",
-    authDomain: "instagram-clone-976fb.firebaseapp.com",
-    projectId: "instagram-clone-976fb",
-    storageBucket: "instagram-clone-976fb.appspot.com",
-    messagingSenderId: "889786593015",
-    appId: "1:889786593015:web:a1000be9076358f076adf2",
-    measurementId: "G-CZQK54TN6X"
+   Your firebase config
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
